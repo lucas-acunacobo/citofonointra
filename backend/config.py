d4344f154337d2f3d58c7e37558e9bf5238cebd1
Lucas Acuña <acuna.cobo@gmail.com>
2025-09-09 13:45:53 -0300

    configuraciones docker

--- backend/config.py
+++ backend/config.py
@@ -8,10 +8,13 @@ class Config:
     DB_PASSWORD = os.environ.get('DB_PASSWORD', 'tu-contraseña-segura')
     DB_HOST = os.environ.get('DB_HOST', 'localhost')
     DB_NAME = os.environ.get('DB_NAME', 'citofono_intra')
-    DB_PORT = os.environ.get('DB_PORT')
+    DB_PORT = os.environ.get('DB_PORT', '5432')
 
+    #SQLALCHEMY_DATABASE_URI = (
+    #    f'postgresql://citofono_intra_user:oaJ2PKm6SHk5N34pO1jnIRWuBgwZjOCl@dpg-d2uh706r433s73ea2l20-a.oregon-postgres.render.com/citofono_intra'
+    #)
     SQLALCHEMY_DATABASE_URI = (
-        f'postgresql://citofono_intra_user:oaJ2PKm6SHk5N34pO1jnIRWuBgwZjOCl@dpg-d2uh706r433s73ea2l20-a.oregon-postgres.render.com/citofono_intra'
+        f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 """
